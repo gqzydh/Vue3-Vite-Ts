3308bbc6142aa633ed44422f1afd1a7884b80f73
庆云 <gqzydh@qq.com>
2021-06-04 14:25:42 +0800

    vue3

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import ElementPlus from 'element-plus'
 
-createApp(App).mount('#app')
+
+import 'element-plus/lib/theme-chalk/index.css'
+
+createApp(App).use(ElementPlus).mount('#app')
---
+++ src/views/axios.vue
@@ -0,0 +1,17 @@
+<template></template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import axios from '../utils/axios'
+
+  export default defineComponent({
+    setup() {
+      axios.get('/users/XPoet')
+        .then((res) => {
+          console.log('res: ', res)
+        })
+        .catch((err) => {
+          console.log('err: ', err)
+        })
+    }
+  })
+</script>
